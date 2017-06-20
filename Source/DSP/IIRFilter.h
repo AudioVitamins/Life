3b50371784d079f6bcf2954f76df88d46d314c34
Jimmy's Window\Jimmy <hung.caovu@gmail.com>
2017-06-21 01:50:54 +0700

    Fix Noise with filter

--- Source/DSP/IIRFilter.h
+++ Source/DSP/IIRFilter.h
@@ -20,6 +20,65 @@ namespace Jimmy {
 			juce::IIRCoefficients mCoefficients;
 
 			ScopedPointer<LinearSmoothedValue<float>> mSmooth;
+
+			//class PlayHead
+			//{
+			//public:
+			//	/** Returns the current gain of the playhead.  */
+			//	float gain()
+			//	{
+			//		auto g = fade.getNextValue();
+			//		return std::sqrt(g);
+			//	}
+			//	bool stopped() const {
+			//		return !(fade.isSmoothing() || fade.getTargetValue() == 1.0f);
+			//	}
+			//	void setFullGain()
+			//	{
+			//		fade.setValue(1.0f);
+			//		resetSmoother();
+			//	}
+			//	void setZeroGain()
+			//	{
+			//		fade.setValue(0.0f);
+			//		resetSmoother();
+			//	}
+			//	void startFadeIn()
+			//	{
+			//		if (fade.getTargetValue() != 1.0f)
+			//		{
+			//			fade.setValue(0.0f);
+			//			resetSmoother();
+			//			fade.setValue(1.0f);
+			//		}
+			//	}
+			//	void startFadeOut()
+			//	{
+			//		if (fade.getTargetValue() != 0.0f)
+			//		{
+			//			fade.setValue(1.0f);
+			//			resetSmoother();
+			//			fade.setValue(0.0f);
+			//		}
+			//	}
+			//	void resetSmoother()
+			//	{
+			//		const float crossfadeTimeInSeconds = 0.25f;
+			//		fade.reset(sampleRate, crossfadeTimeInSeconds);
+			//	}
+			//	void prepareToPlay(float sampleRate_)
+			//	{
+			//		sampleRate = sampleRate_;
+			//	}
+			//	LinearSmoothedValue<float> fade;
+			//	float delayTime{ 0 };
+			//	float sampleRate;
+			//};
+			//PlayHead playheads[2];
+			//int currentPlayhead{ 0 };
+			//int mBlockSize{ 1 };
+			//bool isFirstTimeLoad{ true };
+			//bool loadNewDelayTime{ false };
 		public:
 			IIRFilterLP(float sampleRate, int numChans)
 				:mSampleRate(sampleRate),
@@ -29,22 +88,41 @@ namespace Jimmy {
 				for (int i = 0; i < numChans; i++) {
 					mIIRFilter.add(juce::IIRFilter());
 				}
+
+				/*playheads[0].prepareToPlay(sampleRate);
+				playheads[0].setFullGain();
+				playheads[1].prepareToPlay(sampleRate);
+				playheads[1].setZeroGain();*/
 			};
+
 			~IIRFilterLP() {
 				mIIRFilter.clearQuick();
 			};
 			void changeCutOff(float cutOff) {
 				mSmooth->setValue(cutOff);
-				//mCoefficients = juce::IIRCoefficients::makeLowPass(mSampleRate, cutOff);
-				//for (int i = 0; i < mNumChans; i++) {
-				//	mIIRFilter.getReference(i).setCoefficients(mCoefficients);
-				//}
-			};
-			void reset() {
+				/*mCoefficients = juce::IIRCoefficients::makeLowPass(mSampleRate, cutOff);
 				for (int i = 0; i < mNumChans; i++) {
-					mIIRFilter.getReference(i).reset();
-				}
+					mIIRFilter.getReference(i).setCoefficients(mCoefficients);
+				}*/
+			};
+			/*void togglePlayhead()
+			{
+				currentPlayhead = 1 - currentPlayhead;
 			}
+
+			PlayHead & getOtherPlayhead()
+			{
+				return playheads[1 - currentPlayhead];
+			}
+			void updateDelayTime()
+			{
+				if (getOtherPlayhead().stopped())
+				{
+					playheads[currentPlayhead].startFadeOut();
+					togglePlayhead();
+					playheads[currentPlayhead].startFadeIn();
+				}
+			}*/
 			void process(AudioBuffer<float> &buffer) {
 				float **buffersCh = buffer.getArrayOfWritePointers();
 				int numSamples = buffer.getNumSamples();
@@ -58,8 +136,10 @@ namespace Jimmy {
 					}
 				}
 			};
-			void setSampleRate(float sample) {
-				mSampleRate = sample;
+			void reset() {
+				/*for (int i = 0; i < mNumChans; i++) {
+				mIIRFilter.getReference(i).reset();
+				}*/
 			}
 		};
 		class IIRFilterHP {
@@ -67,6 +147,8 @@ namespace Jimmy {
 			float mSampleRate;
 			int mNumChans;
 			juce::IIRCoefficients mCoefficients;
+
+			ScopedPointer<LinearSmoothedValue<float>> mSmooth;
 		public:
 			IIRFilterHP(float sampleRate, int numChans)
 				:mSampleRate(sampleRate),
@@ -74,32 +156,43 @@ namespace Jimmy {
 				for (int i = 0; i < numChans; i++) {
 					mIIRFilter.add(juce::IIRFilter());
 				}
+				mSmooth = new LinearSmoothedValue<float>();
+				mSmooth->reset(sampleRate, 0.5);
 			};
 			~IIRFilterHP() {
 				mIIRFilter.clearQuick();
 			};
 			void changeCutOff(float cutOff) {
-				mCoefficients = juce::IIRCoefficients::makeHighPass(mSampleRate, cutOff);
-				for (int i = 0; i < mNumChans; i++) {
-					mIIRFilter.getReference(i).setCoefficients(mCoefficients);
-				}
+				mSmooth->setValue(cutOff);
+				//mCoefficients = juce::IIRCoefficients::makeHighPass(mSampleRate, cutOff);
+				//for (int i = 0; i < mNumChans; i++) {
+				//	mIIRFilter.getReference(i).setCoefficients(mCoefficients);
+				//}
 			};
 			void process(AudioBuffer<float> &buffer) {
-				float **buffersCh = buffer.getArrayOfWritePointers();
+				/*float **buffersCh = buffer.getArrayOfWritePointers();
 				int numSamples = buffer.getNumSamples();
-
 				for (int i = 0; i < mNumChans; i++) {
 					mIIRFilter.getReference(i).processSamples(buffersCh[i], numSamples);
+				}*/
+
+				float **buffersCh = buffer.getArrayOfWritePointers();
+				int numSamples = buffer.getNumSamples();
+				for (int i = 0; i < numSamples; i++) {
+
+					float freq = mSmooth->getNextValue();
+					mCoefficients = juce::IIRCoefficients::makeHighPass(mSampleRate, freq);
+					for (int c = 0; c < mNumChans; c++) {
+						mIIRFilter.getReference(c).setCoefficients(mCoefficients);
+						buffersCh[c][i] = mIIRFilter.getReference(c).processSingleSampleRaw(buffersCh[c][i]);
+					}
 				}
 
 			};
-			void setSampleRate(float sample) {
-				mSampleRate = sample;
-			}
 			void reset() {
-				for (int i = 0; i < mNumChans; i++) {
+				/*for (int i = 0; i < mNumChans; i++) {
 					mIIRFilter.getReference(i).reset();
-				}
+				}*/
 			}
 		};
 	};
